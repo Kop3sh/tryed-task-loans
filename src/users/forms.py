cfd417364335f14d3fe9f73472224f58ffaa0ae9
Kop3sh <kirollosadel24@yahoo.com>
2024-10-05 00:03:01 +0300

    fix: remove first and last name (non-existant) from createUserForm

--- src/users/forms.py
+++ src/users/forms.py
@@ -7,13 +7,11 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("email","first_name","last_name","is_staff",)
+        fields = ("email","is_staff",)
     
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
         self.fields['email'].required = True
-        self.fields['first_name'].required = True
-        self.fields['last_name'].required = True
 
 class CustomUserChangeForm(UserChangeForm):
     pass
