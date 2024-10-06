673e7c2f3f1e0ce4b0c7dcc635dc0e31c5b31395
Kop3sh <kirollosadel24@yahoo.com>
2024-10-06 12:58:33 +0300

    add CSRF trusted origins

--- src/treyd/settings.py
+++ src/treyd/settings.py
@@ -184,6 +184,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS").split(",")
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
