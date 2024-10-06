0ee0e7c6621f9fcf6621f5e17a9ddbdf8c601eb7
Kop3sh <kirollosadel24@yahoo.com>
2024-10-07 00:06:06 +0300

    fix tests

--- src/tests/test_loan_request_detail_view.py
+++ src/tests/test_loan_request_detail_view.py
@@ -13,13 +13,21 @@ def test_unauth_user_cannot_get_loan_request_detail(client, test_loan_request):
     assert LoanRequest.objects.all().count() == 1
     assert res.status_code == 401
 
-def test_auth_user_can_get_his_own_loan_request_detail(auth_client, test_loan_request):
+def test_auth_user_can_get_his_own_approved_loan_request_details(auth_client, test_loan_request):
     res = auth_client.get(loan_endpoint + "1/")
+
+    LoanRequest.objects.all().first().status = "approved"
 
     assert LoanRequest.objects.all().count() == 1
     assert res.status_code == 200
     assert Decimal(res.data["amount"]) == test_loan_request.amount
     assert Decimal(res.data["term"]) == test_loan_request.term
+
+def test_auth_user_cannot_get_his_own_pending_loan_request_detail(auth_client, test_loan_request):
+    res = auth_client.get(loan_endpoint + "1/")
+
+    assert LoanRequest.objects.all().count() == 1
+    assert res.status_code == 400
 
 def test_auth_user_cannot_get_other_users_loan_request_detail(other_auth_client, test_loan_request):
     res = other_auth_client.get(loan_endpoint + "1/")
